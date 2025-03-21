7788724d3b930a57f45ea17eb4e668ac2990d787
liwm <liwm@ikongjian.com>
2025-03-21 17:58:48 +0800

    first commit

--- sk-auth/src/main/java/com/liwm/sk/auth/SkAuthApplication.java
+++ sk-auth/src/main/java/com/liwm/sk/auth/SkAuthApplication.java
@@ -16,7 +16,6 @@ public class SkAuthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SkAuthApplication.class, args);
-        System.out.println("启动成功，Sa-Token 配置如下：" + SaManager.getConfig());
     }
 
 }
